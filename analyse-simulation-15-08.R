5bc8d1d6331bd1ecd225edb13ed1e3e009d55cba
tobiriebe <tobi-riebe@hotmail.de>
2016-09-01 07:15:19 +0100

    Update analyse-simulation-15-08.R

--- analyse-simulation-15-08.R
+++ analyse-simulation-15-08.R
@@ -537,7 +537,7 @@ convergenceIteration <- function(dataFile){
   rbind(nred, nredNoiseX, nredNoiseY, nredNoiseXNoiseY, nredAUC, nredAUCNoiseX, nredAUCNoiseY, nredAUCNoiseXNoiseY) #save values as matrix to use them analyse function
 }#end function
 
-setwd("/naslx/projects/ua341/di49suy/sampled-boosting-test-5/mytest-files/jobs/02")
+setwd("/naslx/projects/ua341/di49suy/sampled-boosting-test-5/mytest-files/jobs/01")
 load("result.RData")
 analyseSimulation(result)
 
